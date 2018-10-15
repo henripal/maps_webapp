d7200c440da6ddd7d15110b17e191d4f5d91d37b
Henri Palacci <henri.palacci@gmail.com>
2018-10-15 00:37:34 -0400

    now with all test passing and hooks pup

--- frontend/tests/unit/example.spec.js
+++ frontend/tests/unit/example.spec.js
@@ -3,9 +3,9 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
-    const msg = 'newmessage'
+    const msg = 'new message'
     const wrapper = shallowMount(HelloWorld, {
-      propsData: { "wowi" },
+      propsData: { msg },
       mocks: {
         $http: {get:
           jest.fn(() => Promise.resolve({data: {}}))

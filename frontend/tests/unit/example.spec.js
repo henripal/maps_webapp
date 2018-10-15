41d07fb2786b32c655f29738cd23e5977e2ab1ee
Henri Palacci <henri.palacci@gmail.com>
2018-10-15 00:27:28 -0400

    tyring

--- frontend/tests/unit/example.spec.js
+++ frontend/tests/unit/example.spec.js
@@ -3,7 +3,7 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
-    const msg = 'new message'
+    const msg = 'newmessage'
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg },
       mocks: {
